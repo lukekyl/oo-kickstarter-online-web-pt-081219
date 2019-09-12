275aa9d631d626bdcfe9e9c245e9829bc9448c9f
Kyle Luke <th.oldschool@gmail.com>
2019-09-12 01:08:14 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,9 @@
 class Backer
   attr_accessor
-  attr_reader :backer_projects
+  attr_reader :backer_projects, :name
   
   def initialize(name)
+    @name = name
     @backer_projects = []
   end
 
